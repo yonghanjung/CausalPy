2620abe2b6fdf0910e69f893e9eece2e101c1c3d
Yonghan Jung <yhansjung@gmail.com>
2024-09-02 14:00:38 -0400

    240902 est_general.py

--- est_general.py
+++ est_general.py
@@ -380,6 +380,7 @@ def estimate_Tian(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize = 200
 
 	return ATE
 
+######## 240830 Change with ATE[estimator] HERE
 def estimate_gTian(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize = 200, EB_boosting = 5):
 	np.random.seed(int(seednum))
 	random.seed(int(seednum))
@@ -393,8 +394,9 @@ def estimate_gTian(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize = 20
 	V_Y = sorted( list( set(topo_V) - set(Y)), key=lambda x: topo_V.index(x) )	
 
 	X_values_combinations = pd.DataFrame(product(*[np.unique(obs_data[Vi]) for Vi in X]), columns=X)
-	
+
 	list_estimators = ["OM"] if only_OM else ["OM", "IPW", "DML"]
+	
 	ATE = {}
 	for estimator in list_estimators:
 		ATE[estimator] = {}
@@ -424,9 +426,13 @@ def estimate_gTian(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize = 20
 	marginalized_item_list = list(set(V_SX + [item for sublist in S_Xi_list for item in sublist]) - set(X) - set(Y))
 
 	for _, x_val in X_values_combinations.iterrows():
-		ATE[tuple(x_val)] = 0
+		for estimator in list_estimators:
+			ATE[estimator][tuple(x_val)] = 0
+
 		for marginalized_value in obs_data[marginalized_item_list].drop_duplicates().itertuples(index=False):
-			Q_VX_val = 1 
+			Q_VX_val = {}
+			for estimator in list_estimators:
+				Q_VX_val[estimator] = 1 
 			# Compute Q[V\SX](v)
 			PA_V_SX = graph.find_parents(G, V_SX)
 			# di is the realization of Di, defined as follow: For a portion Di \intersect D_minus_Y, take its value from d_minus_y. For Di \setminus D_minus_Y, take the value from y_val.
@@ -441,8 +447,9 @@ def estimate_gTian(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize = 20
 				for variable in PA_V_SX
 			]
 			# Compute Q[V\SX] 
-			Q_V_SX_val, _, _, _  = est_mSBD.estimate_mSBD_xval_yval(G, PA_V_SX, V_SX, pa_v_sx, v_sx, obs_data, alpha_CI = 0.05, variance_threshold = 100, estimators = estimators)
-			Q_VX_val *= Q_V_SX_val
+			Q_V_SX_val, _, _, _  = est_mSBD.estimate_mSBD_xval_yval(G, PA_V_SX, V_SX, pa_v_sx, v_sx, obs_data, only_OM = only_OM, seednum = seednum, EB_samplesize = EB_samplesize, EB_boosting = EB_boosting)
+			for estimator in list_estimators:
+				Q_VX_val[estimator] *= Q_V_SX_val[estimator]
 
 			for idx in range(len(S_Xi_list)):
 				SXi = S_Xi_list[idx]
@@ -463,9 +470,12 @@ def estimate_gTian(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize = 20
 					for variable in PA_SXi_XCi
 				]
 				# Compute Q[SX_X] 
-				Q_SXi_Xci_val, _, _, _  = est_mSBD.estimate_mSBD_xval_yval(G, PA_SXi_XCi, SXi_XCi, pa_sx_x, sxi_xci, obs_data, alpha_CI = 0.05, variance_threshold = 100, estimators = estimators)
-				Q_VX_val *= Q_SXi_Xci_val
-			ATE[tuple(x_val)] += Q_VX_val
+				Q_SXi_Xci_val, _, _, _  = est_mSBD.estimate_mSBD_xval_yval(G, PA_SXi_XCi, SXi_XCi, pa_sx_x, sxi_xci, obs_data, only_OM = only_OM, seednum = seednum, EB_samplesize = EB_samplesize, EB_boosting = EB_boosting)
+				for estimator in list_estimators:
+					Q_VX_val[estimator] *= Q_SXi_Xci_val[estimator]
+
+			for estimator in list_estimators:
+				ATE[estimator][tuple(x_val)] += Q_VX_val[estimator]
 
 	return ATE
 
@@ -487,16 +497,21 @@ def estimate_product_QD(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize
 	D_minus_Y = list(set(D) - set(Y))
 
 	X_values_combinations = pd.DataFrame(product(*[np.unique(obs_data[Vi]) for Vi in X]), columns=X)
-	
+
 	list_estimators = ["OM"] if only_OM else ["OM", "IPW", "DML"]
+	
 	ATE = {}
 	for estimator in list_estimators:
 		ATE[estimator] = {}
 
 	for _, x_val in X_values_combinations.iterrows():
-		ATE[tuple(x_val)] = 0
+		for estimator in list_estimators:
+			ATE[estimator][tuple(x_val)] = 0
+
 		for d_minus_y in obs_data[D_minus_Y].drop_duplicates().itertuples(index=False):
-			Q_D_val = 1 
+			Q_D_val = {}
+			for estimator in list_estimators:
+				Q_D_val[estimator] = 1 
 			for adj_dict_component in adj_dict_components.values():
 				Di = adj_dict_component[-1]
 				Xi = graph.find_parents(G, Di)
@@ -514,17 +529,20 @@ def estimate_product_QD(G, X, Y, y_val, obs_data, only_OM = False, EB_samplesize
 				]
 
 				# Compute Q[Di] := P(di | do(xi)) 
-				Q_Di_val, _, _, _  = est_mSBD.estimate_mSBD_xval_yval(G, Xi, Di, xi, di, obs_data, alpha_CI = 0.05, variance_threshold = 100, estimators = estimators)
+				Q_Di_val, _, _, _  = est_mSBD.estimate_mSBD_xval_yval(G, Xi, Di, xi, di, obs_data, only_OM = only_OM, seednum = seednum, EB_samplesize = EB_samplesize, EB_boosting = EB_boosting)
 
-				Q_D_val *= Q_Di_val
+				for estimator in list_estimators:
+					Q_D_val[estimator] *= Q_Di_val[estimator]
 			
-			ATE[tuple(x_val)] += Q_D_val
+			for estimator in list_estimators:
+				ATE[estimator][tuple(x_val)] += Q_D_val[estimator]
 	return ATE
 
 
 if __name__ == "__main__":
 	# Generate random SCM and preprocess the graph
 	seednum = int(time.time())
+	# seednum = 1725219547 # only gTian holds. But General performed better 
 
 	print(f'Random seed: {seednum}')
 	np.random.seed(seednum)
@@ -536,9 +554,9 @@ if __name__ == "__main__":
 		condition_BD=False, 
 		condition_mSBD=False, 
 		condition_FD=False, 
-		condition_Tian=True, 
+		condition_Tian=False, 
 		condition_gTian=True,
-		condition_product = True, 
+		condition_product = False, 
 		discrete = True, 
 		seednum = seednum 
 	)
@@ -557,7 +575,7 @@ if __name__ == "__main__":
 	satisfied_gTian = tian.check_Generalized_Tian_criterion(G, X)
 	satisfied_product = tian.check_product_criterion(G, X, Y)
 
-	print(identify.causal_identification(G, X, Y, False))
+	print(identify.causal_identification(G, X, Y, True))
 	# identify.draw_AC_tree(G,X,Y)
 
 	truth = statmodules.ground_truth(scm, obs_data, X, Y)
@@ -574,32 +592,12 @@ if __name__ == "__main__":
 
 	performance_table, rank_correlation_table = statmodules.compute_performance(truth, ATE)
 
+	# ATE_gen = estimate_general(G, X, Y, y_val, obs_data, only_OM = False)
+	# performance_table_gen, rank_correlation_table_gen = statmodules.compute_performance(truth, ATE_gen)
+	# print(performance_table_gen)
+
 	print("Performance")
 	print(performance_table)
 
 	print("Rank Correlation")
 	print(rank_correlation_table)
-
-	
-
-		# ATE_gen = estimate_general(G, X, Y, y_val, obs_data, only_OM = False)
-		# ATE_DML_gen = estimate_general(G, X, Y, y_val, obs_data, alpha_CI = 0.05, variance_threshold = 5, estimators = "DML")
-
-	# performance_OM = np.mean(np.abs(np.array(list(truth.values())) - np.array(list(ATE_OM.values()))))
-	# performance_DML = np.mean(np.abs(np.array(list(truth.values())) - np.array(list(ATE_DML.values()))))
-	# print("Performance (OM):", performance_OM)
-	# print("Performance (DML):", performance_DML)
-
-	# # performance_OM_gen = np.mean(np.abs(np.array(list(truth.values())) - np.array(list(ATE_OM_gen.values()))))
-	# # performance_DML_gen = np.mean(np.abs(np.array(list(truth.values())) - np.array(list(ATE_DML_gen.values()))))
-	# # print("Performance (OM_gen):", performance_OM_gen)
-	# # print("Performance (DML_gen):", performance_DML_gen)
-
-	# rank_correlation, rank_p_values = spearmanr(list(truth.values()), list(ATE_OM.values()))
-	# print(f"Spearman Rank correlation coefficient (OM): {rank_correlation}")
-	# print(f"P-value (OM): {rank_p_values}")
-
-	# rank_correlation, rank_p_values = spearmanr(list(truth.values()), list(ATE_DML.values()))
-	# print(f"Spearman Rank correlation coefficient (DML): {rank_correlation}")
-	# print(f"P-value: {rank_p_values}")
-
